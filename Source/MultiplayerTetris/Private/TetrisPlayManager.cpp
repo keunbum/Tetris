f9f990572bde064be8615a575f723e9b0a4a513d
keunbum <rkeunbum@gmail.com>
2024-06-06 15:04:23 +0900

    Update TetrisPlayManager

--- Source/MultiplayerTetris/Private/TetrisPlayManager.cpp
+++ Source/MultiplayerTetris/Private/TetrisPlayManager.cpp
@@ -3,25 +3,120 @@
 
 #include "TetrisPlayManager.h"
 
+#include "Board.h"
+#include "TetrisGameModeBase.h"
+#include "Tetrimino.h"
+
 // Sets default values
 ATetrisPlayManager::ATetrisPlayManager()
+	: NormalFallSpeed(-1.0f)
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void ATetrisPlayManager::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	Initialize();
 }
 
 // Called every frame
-void ATetrisPlayManager::Tick(float DeltaTime)
+void ATetrisPlayManager::Tick(const float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
+void ATetrisPlayManager::StartMovement(const FVector2D& InMovementDirection)
+{
+	SetMovementDirection(InMovementDirection);
+	MoveToCurrentDirection();
+	SetAutoRepeatMovement();
+}
+
+void ATetrisPlayManager::EndMovement()
+{
+	ClearTimer(AutoRepeatMovementTimerHandle);
+	SetMovementDirection(FVector2D::ZeroVector);
+}
+
+void ATetrisPlayManager::StartSoftDrop()
+{
+	if (TetrominoInPlay)
+	{
+		// NormalFall 일시 중지
+		ClearTimer(NormalFallTimerHandle);
+
+		GetWorldTimerManager().SetTimer(SoftDropTimerHandle, this, &ATetrisPlayManager::MoveDown, GameMode->GetSoftDropSpeed(), bSoftDropTimerLoop, SoftDropTimerInitialDelay);
+	}
+}
+
+void ATetrisPlayManager::EndSoftDrop()
+{
+	ClearTimer(SoftDropTimerHandle);
+
+	// NormalFall 재개
+	SetNormalFallTimer();
+}
+
+void ATetrisPlayManager::StartHardDrop()
+{
+	// TODO: 하드 드롭 로직 추가
+}
+
+void ATetrisPlayManager::StartRotate(const int32 RotationDirection)
+{
+	if (TetrominoInPlay)
+	{
+		TetrominoInPlay->RotateTo(RotationDirection);
+	}
+}
+
+void ATetrisPlayManager::Initialize()
+{
+	GameMode = GetWorld()->GetAuthGameMode<ATetrisGameModeBase>();
+	SetNormalFallTimer();
+	SetMovementDirection(FVector2D::ZeroVector);
+}
+
+void ATetrisPlayManager::ClearTimer(FTimerHandle& InOutTimerHandle)
+{
+	GetWorldTimerManager().ClearTimer(InOutTimerHandle);
+}
+
+void ATetrisPlayManager::MoveTo(const FVector2D& Direction)
+{
+	if (TetrominoInPlay)
+	{
+		const bool bIsNextPositionPossible = true;
+		if (bIsNextPositionPossible)
+		{
+			TetrominoInPlay->Move(Direction);
+		}
+	}
+}
+
+void ATetrisPlayManager::MoveToCurrentDirection()
+{
+	MoveTo(MovementDirection);
+}
+
+void ATetrisPlayManager::MoveDown()
+{
+	MoveTo(ATetrimino::MoveDirectionDown);
+}
+
+void ATetrisPlayManager::SetAutoRepeatMovement()
+{
+	GetWorldTimerManager().SetTimer(AutoRepeatMovementTimerHandle, this, &ATetrisPlayManager::MoveToCurrentDirection, AutoRepeatMovementInterval, bIsAutoRepeatMovementLoop, AutoRepeatMovementInitialDelay);
+}
+
+void ATetrisPlayManager::SetNormalFallTimer()
+{
+	if (GameMode && !GameMode->bNormalFallOff)
+	{
+		GetWorldTimerManager().SetTimer(NormalFallTimerHandle, this, &ATetrisPlayManager::MoveDown, NormalFallSpeed, bIsNormalFallTimerLoop, NormalFallTimerInitialDelay);
+	}
+}
--- Source/MultiplayerTetris/Public/TetrisPlayManager.h
+++ Source/MultiplayerTetris/Public/TetrisPlayManager.h
@@ -6,12 +6,16 @@
 #include "GameFramework/Actor.h"
 #include "TetrisPlayManager.generated.h"
 
+class ATetrimino;
+class ATetrisGameModeBase;
+class UInputComponent;
+
 UCLASS()
 class MULTIPLAYERTETRIS_API ATetrisPlayManager : public AActor
 {
 	GENERATED_BODY()
 	
-public:	
+public:
 	// Sets default values for this actor's properties
 	ATetrisPlayManager();
 
@@ -19,8 +23,66 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	virtual void Tick(const float DeltaTime) override;
 
+public:
+	void SetTetrominoInPlay(ATetrimino* const NewTetrominoInPlay) { TetrominoInPlay = NewTetrominoInPlay; }
+	void SetNormalFallSpeed(const float NewNormalFallSpeed) { NormalFallSpeed = NewNormalFallSpeed; }
+
+	FVector2D GetMovementDirection() const { return MovementDirection; }
+
+	// Event Handlers
+	void StartMovement(const FVector2D& InMovementDirection);
+	void EndMovement();
+	void StartSoftDrop();
+	void EndSoftDrop();
+	void StartHardDrop();
+	/**
+	* @param RotationDirection - +1: 시계 방향, -1: 반시계 방향.
+	*/
+	void StartRotate(const int32 RotationDirection);
+
+private:
+	void Initialize();
+	void ClearTimer(FTimerHandle& InOutTimerHandle);
+
+	void MoveTo(const FVector2D& Direction);
+	void MoveToCurrentDirection();
+	void MoveDown();
+
+	void SetAutoRepeatMovement();
+	void SetNormalFallTimer();
+
+	void SetMovementDirection(const FVector2D& NewMovementDirection) { MovementDirection = NewMovementDirection; }
+
+private:
+	static constexpr bool bIsNormalFallTimerLoop = true;
+	static constexpr float NormalFallTimerInitialDelay = 0.0f;
+
+	static constexpr bool bSoftDropTimerLoop = true;
+	static constexpr float SoftDropTimerInitialDelay = 0.0f;
+
+	static constexpr bool bIsAutoRepeatMovementLoop = true;
+	static constexpr float AutoRepeatMovementInitialDelay = 0.3f;
+	static constexpr float AutoRepeatMovementInterval = 0.05f; // Adjust this value as needed
+
+private:
+	float NormalFallSpeed;
+
+	UPROPERTY()
+	TObjectPtr<ATetrisGameModeBase> GameMode;
+
+	// 조작 중인 테트로미노
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<ATetrimino> TetrominoInPlay;
+
+	// 타이머 핸들러
+	FTimerHandle NormalFallTimerHandle;
+	FTimerHandle SoftDropTimerHandle;
+	FTimerHandle AutoRepeatMovementTimerHandle;
+
+	UPROPERTY(VisibleAnywhere)
+	FVector2D MovementDirection;
 };
