b9e3b36f3b6eed3fb0e8835bd4e9ff1f198a3a59
keunbum <rkeunbum@gmail.com>
2024-06-26 18:09:18 +0900

    Create GhostPiece class

---
+++ Source/MultiplayerTetris/Public/GhostPiece.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tetrimino.h"
+#include "GhostPiece.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTETRIS_API AGhostPiece : public ATetrimino
+{
+	GENERATED_BODY()
+	
+};
