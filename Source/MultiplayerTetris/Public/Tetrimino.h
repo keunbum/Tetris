ae330caec0bcd57f8883609fccf4d16120c9827d
keunbum <rkeunbum@gmail.com>
2024-07-01 11:55:29 +0900

    Create Tetrimino class file (.h, .cpp)

---
+++ Source/MultiplayerTetris/Public/Tetrimino.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TetriminoBase.h"
+#include "Tetrimino.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTETRIS_API ATetrimino : public ATetriminoBase
+{
+	GENERATED_BODY()
+	
+};
