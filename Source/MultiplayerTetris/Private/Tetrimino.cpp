22c208bfd7f07c68feb913d1c28b19cb868103a0
keunbum <rkeunbum@gmail.com>
2024-07-02 13:39:22 +0900

    Update Tetrimino
    저작권 주석 추가

--- Source/MultiplayerTetris/Private/Tetrimino.cpp
+++ Source/MultiplayerTetris/Private/Tetrimino.cpp
@@ -1,5 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// Copyright Ryu KeunBeom. All Rights Reserved.
 
 #include "Tetrimino.h"
 
--- Source/MultiplayerTetris/Public/Tetrimino.h
+++ Source/MultiplayerTetris/Public/Tetrimino.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Ryu KeunBeom. All Rights Reserved.
 
 #pragma once
 
