b952e3ac852c4ef94f5c0e78f1b720d11f2e2b63
keunbum <rkeunbum@gmail.com>
2024-07-08 13:20:36 +0900

    Update Board.cpp
    GetFinalFallingMatrixLocation while문으로 수정

--- Source/MultiplayerTetris/Private/Board.cpp
+++ Source/MultiplayerTetris/Private/Board.cpp
@@ -89,20 +89,11 @@ FIntPoint ABoard::GetFinalFallingMatrixLocation(const ATetrimino* Tetrimino) con
 
 	FIntPoint FinalFallingMatrixLocation = Tetrimino->GetMatrixLocation();
 	const TArray<FIntPoint>& MinoMatrixLocalLocations = Tetrimino->GetMinoMatrixLocalLocations();
-	//while (IsMinoLocationsPossible(FinalFallingMatrixLocation, MinoMatrixLocalLocations))
-	const int32 MaxLoopCount = TotalHeight;
-	int32 LoopCount;
-	for (LoopCount = 0; LoopCount < MaxLoopCount; ++LoopCount)
+	static const FIntPoint MovementIntPoint2D = ATetriminoBase::GetMovementIntPoint2D(ATetrimino::MoveDirectionDown);
+	while (IsMinoLocationsPossible(FinalFallingMatrixLocation + MovementIntPoint2D, MinoMatrixLocalLocations))
 	{
-		static const FIntPoint MovementIntPoint2D = ATetriminoBase::GetMovementIntPoint2D(ATetrimino::MoveDirectionDown);
-		if (!IsMinoLocationsPossible(FinalFallingMatrixLocation + MovementIntPoint2D, MinoMatrixLocalLocations))
-		{
-			break;
-		}
 		FinalFallingMatrixLocation += MovementIntPoint2D;
 	}
-	UE_LOG(LogTemp, Display, TEXT("LoopCount: %d"), LoopCount);
-	UE_LOG(LogTemp, Display, TEXT("FinalFallingMatrixLocation: %s"), *FinalFallingMatrixLocation.ToString());
 	return FinalFallingMatrixLocation;
 }
 
