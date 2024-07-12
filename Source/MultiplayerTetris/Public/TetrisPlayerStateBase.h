502010db1c0a9845f918810c1a628e8a7e077a1d
keunbum <rkeunbum@gmail.com>
2024-07-12 17:10:10 +0900

    Create ATetrisPlayerStateBase class file

---
+++ Source/MultiplayerTetris/Public/TetrisPlayerStateBase.h
@@ -0,0 +1,17 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "TetrisPlayerStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTETRIS_API ATetrisPlayerStateBase : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
