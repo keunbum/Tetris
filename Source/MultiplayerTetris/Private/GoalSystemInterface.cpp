54938166719a502c3ba2158f57bcdc6cb75bfc4a
keunbum <rkeunbum@gmail.com>
2024-07-13 16:11:50 +0900

    Create UGoalSystemInterface interface file

---
+++ Source/MultiplayerTetris/Private/GoalSystemInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+
+#include "GoalSystemInterface.h"
+
+// Add default functionality here for any IGoalSystemInterface functions that are not pure virtual.
---
+++ Source/MultiplayerTetris/Public/GoalSystemInterface.h
@@ -0,0 +1,24 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "GoalSystemInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UGoalSystemInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class MULTIPLAYERTETRIS_API IGoalSystemInterface
+{
+	GENERATED_BODY()
+
+public:
+};
