1be17108f2430cda09f2c4ff88b67a23666f0dd8
keunbum <rkeunbum@gmail.com>
2024-07-13 16:49:38 +0900

    Create UFixedGoalSystem class

---
+++ Source/MultiplayerTetris/Public/FixedGoalSystem.h
@@ -0,0 +1,20 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+
+#include "GoalSystemInterface.h"
+
+#include "FixedGoalSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTETRIS_API UFixedGoalSystem : public UObject, public IGoalSystemInterface
+{
+	GENERATED_BODY()
+	
+};
