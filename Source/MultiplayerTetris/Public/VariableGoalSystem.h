19ac68033cb3b0fb4b469a98218e25eadf9712aa
keunbum <rkeunbum@gmail.com>
2024-07-14 13:04:26 +0900

    Create VariableGoalSystem class file

---
+++ Source/MultiplayerTetris/Public/VariableGoalSystem.h
@@ -0,0 +1,17 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "VariableGoalSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTETRIS_API UVariableGoalSystem : public UObject
+{
+	GENERATED_BODY()
+	
+};
