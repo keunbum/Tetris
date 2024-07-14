e339f0f0d5774b35616ec8339783550ffed7e11b
keunbum <rkeunbum@gmail.com>
2024-07-14 13:06:19 +0900

    Update VariableGoalSystem.h
    IGoalSystemInterface 인터페이스 상속 받기

--- Source/MultiplayerTetris/Public/VariableGoalSystem.h
+++ Source/MultiplayerTetris/Public/VariableGoalSystem.h
@@ -4,13 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+
+#include "GoalSystemInterface.h"
+
 #include "VariableGoalSystem.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class MULTIPLAYERTETRIS_API UVariableGoalSystem : public UObject
+class MULTIPLAYERTETRIS_API UVariableGoalSystem : public UObject, public IGoalSystemInterface
 {
 	GENERATED_BODY()
 	
