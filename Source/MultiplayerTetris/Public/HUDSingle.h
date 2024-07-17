d4b57f2ee0fbc4970aaa2054fb718b878a2df44f
keunbum <rkeunbum@gmail.com>
2024-07-17 20:21:07 +0900

    Create HUDSingle class file

---
+++ Source/MultiplayerTetris/Public/HUDSingle.h
@@ -0,0 +1,17 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HUDSingle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTETRIS_API UHUDSingle : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
