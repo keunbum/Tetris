f84d111cb7f4239135366da921e3087120f3a32c
keunbum <rkeunbum@gmail.com>
2024-08-07 17:23:38 +0900

    Create MainMenu class file

---
+++ Source/MultiplayerTetris/Public/MainMenu.h
@@ -0,0 +1,17 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTETRIS_API UMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
