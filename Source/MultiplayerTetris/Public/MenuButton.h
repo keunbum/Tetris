e7a7b9863d4cf0fda9038b62b4fbbdf3e8bd57d5
keunbum <rkeunbum@gmail.com>
2024-08-09 17:28:11 +0900

    Create MenuButton class file

---
+++ Source/MultiplayerTetris/Public/MenuButton.h
@@ -0,0 +1,17 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ButtonBase.h"
+#include "MenuButton.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTETRIS_API UMenuButton : public UButtonBase
+{
+	GENERATED_BODY()
+	
+};
