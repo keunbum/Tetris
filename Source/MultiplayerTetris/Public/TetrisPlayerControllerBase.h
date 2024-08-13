4d353a23fe86fd560f06ab00598d2c99bc40f0b6
keunbum <rkeunbum@gmail.com>
2024-08-13 14:47:16 +0900

    Create ATetrisPlayerControllerBase class file

---
+++ Source/MultiplayerTetris/Public/TetrisPlayerControllerBase.h
@@ -0,0 +1,17 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TetrisPlayerControllerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTETRIS_API ATetrisPlayerControllerBase : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
