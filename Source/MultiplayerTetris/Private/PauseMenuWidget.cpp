539e8ad34028d3c72b4eaae00504c69e1a1ec100
keunbum <rkeunbum@gmail.com>
2024-08-20 22:49:59 +0900

    Update UPauseMenuWidget
    빈 이벤트 메서드 생성해놓고 바인딩까지는 구현함.
    - 이제 Pause 시 위젯 띄우도록 구현해보기. (언리얼 문서 참고)

--- Source/MultiplayerTetris/Private/PauseMenuWidget.cpp
+++ Source/MultiplayerTetris/Private/PauseMenuWidget.cpp
@@ -2,3 +2,35 @@
 
 #include "PauseMenuWidget.h"
 
+#include "MenuButton.h"
+
+void UPauseMenuWidget::SetInitialFocus()
+{
+	SetDefaultMenuButtonFocus();
+}
+
+void UPauseMenuWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	SetMenuButtons({ ResumeButton, RestartButton, ExitButton });
+
+	ResumeButton->OnClicked.AddDynamic(this, &UPauseMenuWidget::OnResumeClicked);
+	RestartButton->OnClicked.AddDynamic(this, &UPauseMenuWidget::OnRestartClicked);
+	ExitButton->OnClicked.AddDynamic(this, &UPauseMenuWidget::OnExitClicked);
+}
+
+void UPauseMenuWidget::OnResumeClicked()
+{
+
+}
+
+void UPauseMenuWidget::OnRestartClicked()
+{
+
+}
+
+void UPauseMenuWidget::OnExitClicked()
+{
+
+}
--- Source/MultiplayerTetris/Public/PauseMenuWidget.h
+++ Source/MultiplayerTetris/Public/PauseMenuWidget.h
@@ -14,4 +14,27 @@ class MULTIPLAYERTETRIS_API UPauseMenuWidget : public UMenuWidgetBase
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void SetInitialFocus() override;
+
+protected:
+	virtual void NativeConstruct() override;
+
+private:
+	UFUNCTION()
+	void OnResumeClicked();
+	UFUNCTION()
+	void OnRestartClicked();
+	UFUNCTION()
+	void OnExitClicked();
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UMenuButton> ResumeButton;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UMenuButton> RestartButton;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UMenuButton> ExitButton;
 };
