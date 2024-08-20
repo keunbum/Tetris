52f929defb3e13ffcdb77fe5b3a5be55c927d499
keunbum <rkeunbum@gmail.com>
2024-08-20 14:35:29 +0900

    Update MainMenuWidget.cpp
    코드 묶음 메서드 호출로 대체

--- Source/MultiplayerTetris/Private/MainMenuWidget.cpp
+++ Source/MultiplayerTetris/Private/MainMenuWidget.cpp
@@ -4,11 +4,7 @@
 
 void UMainMenuWidget::SetInitialFocus()
 {
-	// 초기에 포커싱된 버튼 없음.
-	FocusedButtonIndex = UMenuWidgetBase::InvalidButtonIndex;
-	// 위젯 자체에는 포커싱이 걸려야 키보드 입력을 받을 수 있음.
-	bIsFocusable = true;
-	SetFocus();
+	SetWidgetFocusOnly();
 }
 
 void UMainMenuWidget::NativeConstruct()
