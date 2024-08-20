913145dc044582cb345050a1456026f36ee15b76
keunbum <rkeunbum@gmail.com>
2024-08-20 14:23:43 +0900

    Create class file
    PauseMenuWidget

---
+++ Source/MultiplayerTetris/Public/PauseMenuWidget.h
@@ -0,0 +1,17 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MenuWidgetBase.h"
+#include "PauseMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTETRIS_API UPauseMenuWidget : public UMenuWidgetBase
+{
+	GENERATED_BODY()
+	
+};
