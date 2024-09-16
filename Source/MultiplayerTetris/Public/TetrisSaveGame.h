7feb4937f81fc161892811cfd376b997edf6ba48
keunbum <rkeunbum@gmail.com>
2024-09-16 15:43:36 +0900

    Create TetrisSaveGame class file

---
+++ Source/MultiplayerTetris/Public/TetrisSaveGame.h
@@ -0,0 +1,17 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "TetrisSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTETRIS_API UTetrisSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
