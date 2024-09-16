179d675a47366dffa05cec4c6414f032c80fcf57
keunbum <rkeunbum@gmail.com>
2024-09-16 18:29:26 +0900

    Create TetrisGameModeBase class file

---
+++ Source/MultiplayerTetris/Public/TetrisGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TetrisGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTETRIS_API ATetrisGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
