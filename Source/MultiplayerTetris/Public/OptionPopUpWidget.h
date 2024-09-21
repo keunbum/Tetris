c907b6d46d6e5bbcbd38662e97415eaba099175f
keunbum <rkeunbum@gmail.com>
2024-09-21 14:56:47 +0900

    Create class file
    OptionPopUpWidget

---
+++ Source/MultiplayerTetris/Public/OptionPopUpWidget.h
@@ -0,0 +1,17 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PopUpWidgetBase.h"
+#include "OptionPopUpWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTETRIS_API UOptionPopUpWidget : public UPopUpWidgetBase
+{
+	GENERATED_BODY()
+	
+};
