ac0418de0792e2a76dc3405e04ca9b7aa41be7d7
keunbum <rkeunbum@gmail.com>
2024-09-21 14:54:10 +0900

    Create class file
    PopUpWidgetBase

---
+++ Source/MultiplayerTetris/Public/PopUpWidgetBase.h
@@ -0,0 +1,17 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PopUpWidgetBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTETRIS_API UPopUpWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
