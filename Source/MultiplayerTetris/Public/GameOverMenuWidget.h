5cb2397e005c796c912518021c9ad817929e1c7b
keunbum <rkeunbum@gmail.com>
2024-09-25 15:32:29 +0900

    Create class file
    GameOverMenuWidget

---
+++ Source/MultiplayerTetris/Public/GameOverMenuWidget.h
@@ -0,0 +1,17 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MenuWidgetBase.h"
+#include "GameOverMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTETRIS_API UGameOverMenuWidget : public UMenuWidgetBase
+{
+	GENERATED_BODY()
+	
+};
