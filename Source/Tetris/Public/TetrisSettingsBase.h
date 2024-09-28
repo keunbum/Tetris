b6114c974f8259df9eece8eb4073c3625e8035f4
keunbum <rkeunbum@gmail.com>
2024-09-28 15:24:49 +0900

    Create class
    UTetrisSettingsBase

---
+++ Source/Tetris/Public/TetrisSettingsBase.h
@@ -0,0 +1,17 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "TetrisSettingsBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TETRIS_API UTetrisSettingsBase : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
