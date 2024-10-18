cb7c9a7ea56cc692fa89e34bdc93804ec408fca5
keunbum <rkeunbum@gmail.com>
2024-10-18 17:18:36 +0900

    Rename class
    ATetrisSinglePlayerGameMode -> ATetrisGameModeIngameSingle

--- Source/Tetris/Public/TetrisGameModeIngameSingle.h
+++ Source/Tetris/Public/TetrisGameModeIngameSingle.h
@@ -4,15 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "TetrisGameModeIngameBase.h"
-#include "TetrisSinglePlayerGameMode.generated.h"
+#include "TetrisGameModeIngameSingle.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class TETRIS_API ATetrisSinglePlayerGameMode : public ATetrisGameModeIngameBase
+class TETRIS_API ATetrisGameModeIngameSingle : public ATetrisGameModeIngameBase
 {
 	GENERATED_BODY()
 	
-public:
 };
