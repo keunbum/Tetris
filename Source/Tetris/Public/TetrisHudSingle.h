c36f52efb9c993742f617a69084ec729b879874b
keunbum <rkeunbum@gmail.com>
2024-10-20 23:45:52 +0900

    Create TetrisHudSingle class file

---
+++ Source/Tetris/Public/TetrisHudSingle.h
@@ -0,0 +1,17 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TetrisHudBase.h"
+#include "TetrisHudSingle.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TETRIS_API ATetrisHudSingle : public ATetrisHudBase
+{
+	GENERATED_BODY()
+	
+};
