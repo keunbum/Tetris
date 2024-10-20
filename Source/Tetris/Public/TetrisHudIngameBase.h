3cc286f5e607a78678ce4d9514566c38cd9f6fed
keunbum <rkeunbum@gmail.com>
2024-10-20 23:54:02 +0900

    Create TetrisHudIngameBase class file

---
+++ Source/Tetris/Public/TetrisHudIngameBase.h
@@ -0,0 +1,17 @@
+// Copyright Ryu KeunBeom. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TetrisHudBase.h"
+#include "TetrisHudIngameBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class TETRIS_API ATetrisHudIngameBase : public ATetrisHudBase
+{
+	GENERATED_BODY()
+	
+};
