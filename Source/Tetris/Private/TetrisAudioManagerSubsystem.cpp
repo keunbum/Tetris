30c7cc1a77efef63f30e05c0a99e531852846dfc
keunbum <rkeunbum@gmail.com>
2024-10-22 13:40:55 +0900

    Update TetrisAudioManagerSubsystem.cpp
    - if문 구조 개선
    - early return 적용

--- Source/Tetris/Private/TetrisAudioManagerSubsystem.cpp
+++ Source/Tetris/Private/TetrisAudioManagerSubsystem.cpp
@@ -27,42 +27,45 @@ void UTetrisAudioManagerSubsystem::Deinitialize()
 
 void UTetrisAudioManagerSubsystem::SaveSettings()
 {
-	if (GConfig)
+	if (!GConfig)
 	{
-		for (const auto& [SoundClassName, Volume] : SoundClassVolumes)
-		{
-			GConfig->SetFloat(*AudioConfigSectionName.ToString(), *SoundClassName.ToString(), Volume, AudioConfigFileName.ToString());
-		}
-		GConfig->Flush(false, AudioConfigFileName.ToString());
+		UE_LOG(LogTemp, Error, TEXT("%s - Failed to get GConfig"), *FString(__FUNCTION__));
+		return;
 	}
+
+	for (const auto& [SoundClassName, Volume] : SoundClassVolumes)
+	{
+		GConfig->SetFloat(*AudioConfigSectionName.ToString(), *SoundClassName.ToString(), Volume, AudioConfigFileName.ToString());
+	}
+	GConfig->Flush(false, AudioConfigFileName.ToString());
 }
 
 void UTetrisAudioManagerSubsystem::LoadSoundControlDataAsset()
 {
 	// 언리얼 에디터에서 미리 작성한 파일을 읽는다.
-	if (const USoundControlDataAsset* SoundControlDataAsset = LoadObject<USoundControlDataAsset>(nullptr, *SoundControlDataAssetPath.ToString()))
-	{
-		// Set Main Sound Mix
-		MainSoundMix = SoundControlDataAsset->MainSoundMix;
-		if (!MainSoundMix)
-		{
-			UE_LOG(LogTemp, Error, TEXT("%s - Failed to load MainSoundMix"), *FString(__FUNCTION__));
-			return;
-		}
-		AsyncTask(ENamedThreads::GameThread, [this]()
-			{
-				UGameplayStatics::SetBaseSoundMix(GetWorld(), MainSoundMix);
-			});
-
-		// Set Sound Classes
-		MainSoundClass = SoundControlDataAsset->MainSoundClass;
-		BgmSoundClass = SoundControlDataAsset->BgmSoundClass;
-		SfxSoundClass = SoundControlDataAsset->SfxSoundClass;
-	}
-	else
+	const USoundControlDataAsset* SoundControlDataAsset = LoadObject<USoundControlDataAsset>(nullptr, *SoundControlDataAssetPath.ToString());
+	if (!SoundControlDataAsset)
 	{
 		UE_LOG(LogTemp, Error, TEXT("%s - Failed to load SoundControlDataAsset"), *FString(__FUNCTION__));
+		return;
 	}
+
+	// Set Main Sound Mix
+	MainSoundMix = SoundControlDataAsset->MainSoundMix;
+	if (!MainSoundMix)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s - Invalid MainSoundMix"), *FString(__FUNCTION__));
+		return;
+	}
+	AsyncTask(ENamedThreads::GameThread, [this]()
+		{
+			UGameplayStatics::SetBaseSoundMix(GetWorld(), MainSoundMix);
+		});
+
+	// Set Sound Classes
+	MainSoundClass = SoundControlDataAsset->MainSoundClass;
+	BgmSoundClass = SoundControlDataAsset->BgmSoundClass;
+	SfxSoundClass = SoundControlDataAsset->SfxSoundClass;
 }
 
 void UTetrisAudioManagerSubsystem::LoadSettings()
@@ -72,31 +75,33 @@ void UTetrisAudioManagerSubsystem::LoadSettings()
 
 void UTetrisAudioManagerSubsystem::LoadSoundClassVolumeSettings()
 {
-	if (GConfig)
+	if (!GConfig)
 	{
-		for (USoundClass* const SoundClass : { MainSoundClass, BgmSoundClass, SfxSoundClass })
+		UE_LOG(LogTemp, Error, TEXT("%s - Failed to get GConfig"), *FString(__FUNCTION__));
+		return;
+	}
+	
+	for (USoundClass* const SoundClass : { MainSoundClass, BgmSoundClass, SfxSoundClass })
+	{
+		if (!SoundClass)
 		{
-			if (SoundClass)
-			{
-				float Volume = UTetrisAudioManagerSubsystem::DefaultVolume;
-				if (float VolumeSaved;
-					GConfig->GetFloat(*AudioConfigSectionName.ToString(), *SoundClass->GetName(), VolumeSaved, AudioConfigFileName.ToString()))
-				{
-					// 기존에 저장된 값이 있다면 불러온다.
-					Volume = VolumeSaved;
-				}
-				else
-				{
-					// 없다면 새로 저장한다.
-					GConfig->SetFloat(*AudioConfigSectionName.ToString(), *SoundClass->GetName(), Volume, AudioConfigFileName.ToString());
-				}
-				SetSoundClassVolume(SoundClass, Volume);
-			}
-			else
-			{
-				UE_LOG(LogTemp, Error, TEXT("%s - Invalid SoundClass"), *FString(__FUNCTION__));
-			}
+			UE_LOG(LogTemp, Error, TEXT("%s - Invalid SoundClass"), *FString(__FUNCTION__));
+			return;
 		}
+
+		float Volume = UTetrisAudioManagerSubsystem::DefaultVolume;
+		if (float VolumeSaved;
+			GConfig->GetFloat(*AudioConfigSectionName.ToString(), *SoundClass->GetName(), VolumeSaved, AudioConfigFileName.ToString()))
+		{
+			// 기존에 저장된 값이 있다면 불러온다.
+			Volume = VolumeSaved;
+		}
+		else
+		{
+			// 없다면 새로 저장한다.
+			GConfig->SetFloat(*AudioConfigSectionName.ToString(), *SoundClass->GetName(), Volume, AudioConfigFileName.ToString());
+		}
+		SetSoundClassVolume(SoundClass, Volume);
 	}
 }
 
@@ -110,7 +115,6 @@ float UTetrisAudioManagerSubsystem::GetSoundClassVolume(USoundClass* const Sound
 {
 	if (const float* Volume = SoundClassVolumes.Find(SoundClass->GetFName()))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("%s - %s: %f"), *FString(__FUNCTION__), *SoundClass->GetName(), *Volume);
 		return *Volume;
 	}
 
@@ -119,15 +123,14 @@ float UTetrisAudioManagerSubsystem::GetSoundClassVolume(USoundClass* const Sound
 
 void UTetrisAudioManagerSubsystem::SetSoundMixClassOverrideInGameThread(USoundMix* const SoundMix, USoundClass* const SoundClass, const float Volume, const float Pitch, const float FadeInTime, const bool bApplyToChildren)
 {
-	if (SoundClass)
+	if (!SoundClass)
 	{
-		AsyncTask(ENamedThreads::GameThread, [this, SoundMix, SoundClass, Volume, Pitch, FadeInTime, bApplyToChildren]()
-			{
-				UGameplayStatics::SetSoundMixClassOverride(GetWorld(), SoundMix, SoundClass, Volume, Pitch, FadeInTime, bApplyToChildren);
-			});
+		UE_LOG(LogTemp, Error, TEXT("%s - Invalid SoundClass"), *FString(__FUNCTION__));
+		return;
 	}
-	else
-	{
-		UE_LOG(LogTemp, Error, TEXT("%s - Invalid SoundClassName"), *FString(__FUNCTION__));
-	}
+
+	AsyncTask(ENamedThreads::GameThread, [this, SoundMix, SoundClass, Volume, Pitch, FadeInTime, bApplyToChildren]()
+		{
+			UGameplayStatics::SetSoundMixClassOverride(GetWorld(), SoundMix, SoundClass, Volume, Pitch, FadeInTime, bApplyToChildren);
+		});
 }
